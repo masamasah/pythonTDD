dc3422e26dd608a7277cce8a4161bad2326221b4
masayuki.hirata <masayuki.h0729@gmail.com>
2019-01-02 17:00:19 +0900

    Page.166
    testResultを導入した

--- xunit.py
+++ xunit.py
@@ -1,3 +1,14 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -9,10 +20,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
 
 class WasRun(TestCase):
@@ -26,6 +40,9 @@ class WasRun(TestCase):
     def testMethod(self):
         self.log = self.log + "testMethod "
 
+    def testBrokenTest(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
 
@@ -34,5 +51,17 @@ class TestCaseTest(TestCase):
         test.run()
         assert("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenTest")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
